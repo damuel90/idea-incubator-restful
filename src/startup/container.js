e8f89058412340b7dc9406f57a49ce6e6f6645e7
Damuel <damuel90@gmail.com>
2020-05-30 16:49:58 -0430

    add Server, config and container

---
+++ src/startup/container.js
@@ -0,0 +1,82 @@
+const { 
+    createContainer, 
+    asClass, 
+    asValue, 
+    asFunction 
+} = require('awilix');
+// config
+const config = require('../config');
+const app = require('.');
+//services
+const { 
+    HomeService, 
+    UserService, 
+    IdeaService, 
+    CommentService 
+} = require('../services');
+// controller
+const { 
+    HomeController, 
+    UserController, 
+    IdeaController, 
+    CommentController 
+} = require('../controllers');
+// routes
+const Routes = require('../routes');
+const { 
+    HomeRoutes, 
+    UserRoutes, 
+    IdeaRoutes, 
+    CommentRoutes 
+} = require('../routes/index.routes');
+// models
+const { 
+    User, 
+    Idea, 
+    Comment 
+} = require('../models');
+// repositories
+const { 
+    UserRepository, 
+    IdeaRepository, 
+    CommentRepository 
+} = require('../repositories');
+
+const container = createContainer();
+
+container
+    .register({
+        app: asClass(app).singleton(),
+        router: asFunction(Routes).singleton(),
+        config: asValue(config)
+    })
+    .register({
+        HomeService: asClass(HomeService).singleton(),
+        UserService: asClass(UserService).singleton(),
+        IdeaService: asClass(IdeaService).singleton(),
+        CommentService: asClass(CommentService).singleton()
+    })
+    .register({
+        HomeController: asClass(HomeController.bind(HomeController)).singleton(),
+        UserController: asClass(UserController.bind(UserController)).singleton(),
+        IdeaController: asClass(IdeaController.bind(IdeaController)).singleton(),
+        CommentController: asClass(CommentController.bind(CommentController)).singleton()
+    })
+    .register({
+        HomeRoutes: asFunction(HomeRoutes).singleton(),
+        UserRoutes: asFunction(UserRoutes).singleton(),
+        IdeaRoutes: asFunction(IdeaRoutes).singleton(),
+        CommentRoutes: asFunction(CommentRoutes).singleton()
+    })
+    .register({
+        User: asValue(User),
+        Idea: asValue(Idea),
+        Comment: asValue(Comment)
+    })
+    .register({
+        UserRepository: asClass(UserRepository).singleton(),
+        IdeaRepository: asClass(IdeaRepository).singleton(),
+        CommentRepository: asClass(CommentRepository).singleton()
+    })
+
+module.exports = container;
---
+++ src/startup/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+let _express = null;
+let _config = null;
+
+class Server{
+    constructor({ config, router }){
+        _config = config;
+        _express = express().use(router);
+    }
+
+    start(){
+        return new Promise(resolver => {
+            _express.listen(_config.PORT, ()=>{
+                console.log(`${_config.APPLICATION_NAME}: API running on port ${_config.PORT}`);
+
+                resolver();
+            })
+        })
+    }
+}
+
+module.exports = Server;
